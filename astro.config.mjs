c4c006fecc9b65baeab1e2fd3138956b87dcd939
jc301 <jc301@gmail.com>
2023-12-19 16:34:17 -0400

    github workflows deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://jctr3012.github.io',
+    base: '/jctr3012.github.io',
+});
